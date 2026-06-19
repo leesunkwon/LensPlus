6de75a33a38956ee71cad115634b9b8df1d1823e
스윗남 (Kotlinsun) <lsk859797@gmail.com>
2026-06-19 11:30:35 +0900

    [Chore] 프로젝트 초기 설정
    - Xcode 프로젝트 초기 파일을 커밋 대상으로 정리했습니다.
    - 개인 Xcode 상태 파일과 빌드 산출물이 Git에 포함되지 않도록 .gitignore를 추가했습니다.
    - Gemini API 키 관리를 위해 실제 Secrets.xcconfig는 제외하고 예시 파일만 추적하도록 설정했습니다.
    
    Related to: #
    Close: #

---
+++ Lens+/Lens+/Lens_App.swift
@@ -0,0 +1,17 @@
+//
+//  Lens_App.swift
+//  Lens+
+//
+//  Created by sunkwon on 6/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Lens_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
